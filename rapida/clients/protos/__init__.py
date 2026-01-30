af190886970d88e41db86e180733de2bc80a7b2e
Prashant Srivastav <p_srivastav@outlook.com>
2026-01-30 15:40:56 +0800

    fix: regenerated proto artifacts

---
+++ rapida/clients/protos/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+"""Protobuf generated modules for Rapida clients."""
--- rapida/clients/protos/invoker_api_pb2_grpc.py
+++ rapida/clients/protos/invoker_api_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import rapida.clients.protos.invoker_api_pb2 as invoker__api__pb2
 
-GRPC_GENERATED_VERSION = '1.72.1'
+GRPC_GENERATED_VERSION = '1.74.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
